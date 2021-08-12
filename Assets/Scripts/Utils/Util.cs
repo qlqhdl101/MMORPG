6fd4f1aa8d6d3ca05132b2320d84124ff55e1dd0
qlqhdl101 <71308726+qlqhdl101@users.noreply.github.com>
2021-08-12 17:00:34 +0900

    First

---
+++ Assets/Scripts/Utils/Util.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util 
+{
+    public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UniyEngine.Object
+    {
+        if (go == null)
+            return null;
+
+        if (recursive == false)
+        {
+
+        }
+        else
+        {
+
+        }
+        return null;
+    }
+}
